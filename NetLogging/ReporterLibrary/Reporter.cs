6722e3f14dd6c60626b1d1b50638c032e2cba671
Rudolf Dvoracek <rudolf.dvoracek@oxinst.com>
2025-01-31 10:10:13 +0000

    Added code for serilog structured logging

--- NetLogging/ReporterLibrary/Reporter.cs
+++ NetLogging/ReporterLibrary/Reporter.cs
@@ -4,7 +4,6 @@ namespace ReporterLibrary
 {
     public class Reporter
     {
-
         private readonly ILogger<Reporter> _logger;
 
         public Reporter(ILogger<Reporter> logger)
@@ -16,6 +15,9 @@ namespace ReporterLibrary
         {
             for (int i = 0; i < 25000; i++)
             {
+                // Uncomment following lines to get structured logging
+                // var car = new Car();
+                // _logger.LogInformation("Logging information message number {Count} for {@car}", i, car);
                 _logger.LogInformation("Logging information message number {Count}", i);
             }
         }
---
+++ NetLogging/ReporterLibrary/Tyre.cs
@@ -0,0 +1,8 @@
+﻿namespace ReporterLibrary;
+
+public sealed class Tyre
+{
+    private static readonly Random random = new Random();
+
+    public float Inflation => Math.Abs(random.NextSingle());
+}
